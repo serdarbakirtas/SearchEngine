2100f3e324fededacc05097dbe87efe5277dfe6f
Hasan Bakirtas <serdarbakirtas@gmail.com>
2017-10-01 01:06:32 +0300

    BandSearch and BandDetail setup is completed and tableview and uiview created for both.

--- SearchEngine/SearchEngine/AppDelegate.swift
+++ SearchEngine/SearchEngine/AppDelegate.swift
@@ -15,7 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        AppConfig.launch(with: launchOptions ?? [:]) {
+            
+        }
+        
+        let main = R.storyboard.bandSearch.instantiateInitialViewController()
+        
+        window?.rootViewController = main
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
